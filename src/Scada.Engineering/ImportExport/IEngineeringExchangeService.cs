26fe573d7e2c626f0a6118e97e05f0cd156135be
brunolrogerio-collab <brunolrogerio@gmail.com>
2026-08-25 20:57:06 -0300

    feat(engineering): expose data source CSV exchange

--- src/Scada.Engineering/ImportExport/IEngineeringExchangeService.cs
+++ src/Scada.Engineering/ImportExport/IEngineeringExchangeService.cs
@@ -8,10 +8,12 @@ public interface IEngineeringExchangeService
     string ExportJson(bool indented = true);
     string ExportTagsCsv();
     string ExportAlarmsCsv();
+    string ExportDataSourcesCsv();
 
     EngineeringPackage ParseJson(string json);
     EngineeringPackage ParseTagsCsv(string csv);
     EngineeringPackage ParseAlarmsCsv(string csv);
+    EngineeringPackage ParseDataSourcesCsv(string csv);
 
     ImportPreview Preview(EngineeringPackage package, ImportMode mode);
     ImportResult Apply(EngineeringPackage package, ImportMode mode);

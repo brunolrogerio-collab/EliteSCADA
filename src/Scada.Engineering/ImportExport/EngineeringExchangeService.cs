6441c94ef499bc5ed33d692b31b97043f5ad5e56
brunolrogerio-collab <brunolrogerio@gmail.com>
2026-08-26 19:49:19 -0300

    Advance Engineering schema for Internal Memory integration

--- src/Scada.Engineering/ImportExport/EngineeringExchangeService.cs
+++ src/Scada.Engineering/ImportExport/EngineeringExchangeService.cs
@@ -15,7 +15,7 @@ namespace Scada.Engineering.ImportExport;
 public sealed class EngineeringExchangeService : IEngineeringExchangeService
 {
     public const string CurrentSchema = "scada.engineering";
-    public const int CurrentSchemaVersion = 7;
+    public const int CurrentSchemaVersion = 8;
 
     private readonly ITagRegistry _tags;
     private readonly IAlarmEngine _alarms;

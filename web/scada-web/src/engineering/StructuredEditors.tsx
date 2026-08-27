2a828fb68408db03a110839061b58db0ba3893b2
brunolrogerio-collab <brunolrogerio@gmail.com>
2026-08-26 21:33:56 -0300

    feat: surface Internal Memory settings in TAG editor

--- web/scada-web/src/engineering/StructuredEditors.tsx
+++ web/scada-web/src/engineering/StructuredEditors.tsx
@@ -1,1 +1,19 @@
-export { DataSourceEditor, TagEditor } from './EngineeringMutationPanels';
+import React from 'react';
+import {
+  DataSourceEditor,
+  TagEditor as BaseTagEditor
+} from './EngineeringMutationPanels';
+import { MemoryTagSettingsPanel } from './MemoryTagSettingsPanel';
+import type { EngineeringLocale } from './i18n';
+import type { EngineeringPackageView } from './types';
+
+export { DataSourceEditor };
+
+export function TagEditor({ model, locale }: { model: EngineeringPackageView; locale: EngineeringLocale }) {
+  return (
+    <>
+      <BaseTagEditor model={model} locale={locale} />
+      <MemoryTagSettingsPanel model={model} locale={locale} />
+    </>
+  );
+}

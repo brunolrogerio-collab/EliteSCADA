7c9825629b62f064aaabf0292432821c18a55a9c
brunolrogerio-collab <brunolrogerio@gmail.com>
2026-08-28 16:18:52 -0300

    Export built-in visual object schemas

--- web/scada-web/src/visual-runtime/index.ts
+++ web/scada-web/src/visual-runtime/index.ts
@@ -2,5 +2,6 @@ export * from './visualPropertyTypes';
 export * from './visualPropertyRegistry';
 export * from './visualEngineeringProjection';
 export * from './legacyVisualEngineeringPropertyCodec';
+export * from './builtinVisualObjectSchemas';
 export * from './runtimeVisualInstance';
 export * from './visualPythonPropertyCapabilityProvider';

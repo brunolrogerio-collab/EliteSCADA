ffde3b03a3b647acb2f0c484c11b956c602237d6
brunolrogerio-collab <brunolrogerio@gmail.com>
2026-08-28 15:17:28 -0300

    Expose integrated Wave 07 visual runtime surface

--- web/scada-web/src/visual-runtime/index.ts
+++ web/scada-web/src/visual-runtime/index.ts
@@ -1,3 +1,6 @@
 export * from './visualPropertyTypes';
 export * from './visualPropertyRegistry';
 export * from './visualEngineeringProjection';
+export * from './runtimeVisualPropertyPort';
+export * from './runtimeVisualPropertyAdapter';
+export * from './runtimeVisualInstance';

ee9a0b69bc785af4396d365d68a68625d91c8d1e
brunolrogerio-collab <brunolrogerio@gmail.com>
2026-08-28 18:46:53 -0300

    Export canonical visual Engineering property codec

--- web/scada-web/src/visual-runtime/index.ts
+++ web/scada-web/src/visual-runtime/index.ts
@@ -1,6 +1,7 @@
 export * from './visualPropertyTypes';
 export * from './visualPropertyRegistry';
 export * from './visualEngineeringProjection';
+export * from './visualEngineeringPropertyCodec';
 export * from './legacyVisualEngineeringPropertyCodec';
 export * from './builtinVisualObjectSchemas';
 export * from './runtimeVisualInstance';

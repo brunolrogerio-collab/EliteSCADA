9f88992ac61ee820a3501233b0ce6423251e47e6
brunolrogerio-collab <brunolrogerio@gmail.com>
2026-08-28 15:27:17 -0300

    Narrow Wave 07 public visual runtime surface

--- web/scada-web/src/visual-runtime/index.ts
+++ web/scada-web/src/visual-runtime/index.ts
@@ -1,6 +1,5 @@
 export * from './visualPropertyTypes';
 export * from './visualPropertyRegistry';
 export * from './visualEngineeringProjection';
-export * from './runtimeVisualPropertyPort';
-export * from './runtimeVisualPropertyAdapter';
 export * from './runtimeVisualInstance';
+export * from './visualPythonPropertyCapabilityProvider';

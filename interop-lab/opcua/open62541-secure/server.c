f4544d7fcf3c16eab22f2b514e2acd1605ab48cf
brunolrogerio-collab <brunolrogerio@gmail.com>
2026-08-31 02:08:58 -0300

    test(opcua): add encrypted username access control to secure peer

--- interop-lab/opcua/open62541-secure/server.c
+++ interop-lab/opcua/open62541-secure/server.c
@@ -5,6 +5,10 @@
 #define LAB_NAMESPACE_INDEX 2
 static volatile UA_Boolean running = true;
 
+static UA_UsernamePasswordLogin userLogins[1] = {
+    {UA_STRING_STATIC("elite-user"), UA_STRING_STATIC("elite-pass")}
+};
+
 static void stopHandler(int signalNumber) { (void)signalNumber; running = false; }
 
 static UA_ByteString loadFile(const char *path) {
@@ -24,18 +28,17 @@ static UA_ByteString loadFile(const char *path) {
     return data;
 }
 
-static UA_StatusCode addInt32Variable(UA_Server *server) {
-    UA_Int32 initialValue = 7;
+static UA_StatusCode addInt32Variable(UA_Server *server, const char *nodeName, const char *displayName, UA_Int32 initialValue) {
     UA_VariableAttributes attr = UA_VariableAttributes_default;
     UA_Variant_setScalar(&attr.value, &initialValue, &UA_TYPES[UA_TYPES_INT32]);
-    attr.displayName = UA_LOCALIZEDTEXT("en-US", "Secure Counter");
+    attr.displayName = UA_LOCALIZEDTEXT("en-US", (char *)displayName);
     attr.dataType = UA_TYPES[UA_TYPES_INT32].typeId;
     attr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
     return UA_Server_addVariableNode(server,
-        UA_NODEID_STRING(LAB_NAMESPACE_INDEX, "Lab.SecureCounter"),
+        UA_NODEID_STRING(LAB_NAMESPACE_INDEX, (char *)nodeName),
         UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER),
         UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES),
-        UA_QUALIFIEDNAME(LAB_NAMESPACE_INDEX, "Secure Counter"),
+        UA_QUALIFIEDNAME(LAB_NAMESPACE_INDEX, (char *)displayName),
         UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), attr, NULL, NULL);
 }
 
@@ -72,10 +75,24 @@ int main(int argc, char **argv) {
     UA_String_clear(&config->applicationDescription.applicationUri);
     config->applicationDescription.applicationUri = UA_STRING_ALLOC("urn:elitescada:interop:opcua:server");
 
-    UA_UInt16 namespaceIndex = UA_Server_addNamespace(server, "urn:elitescada:interop:opcua");
-    if(namespaceIndex != LAB_NAMESPACE_INDEX || addInt32Variable(server) != UA_STATUSCODE_GOOD) {
+    /* Keep anonymous for the certificate-protected baseline, and expose a deterministic
+       username token only through an encrypted Basic256Sha256 user-token policy. */
+    config->allowNonePolicyPassword = false;
+    config->accessControl.clear(&config->accessControl);
+    const UA_String userTokenPolicy = UA_STRING("http://opcfoundation.org/UA/SecurityPolicy#Basic256Sha256");
+    status = UA_AccessControl_default(config, true, &userTokenPolicy, 1, userLogins);
+    if(status != UA_STATUSCODE_GOOD) {
+        fprintf(stderr, "secure access control configuration failed: %s\n", UA_StatusCode_name(status));
         UA_Server_delete(server);
         return 6;
+    }
+
+    UA_UInt16 namespaceIndex = UA_Server_addNamespace(server, "urn:elitescada:interop:opcua");
+    if(namespaceIndex != LAB_NAMESPACE_INDEX ||
+       addInt32Variable(server, "Lab.SecureCounter", "Secure Counter", 7) != UA_STATUSCODE_GOOD ||
+       addInt32Variable(server, "Lab.UserCounter", "User Counter", 17) != UA_STATUSCODE_GOOD) {
+        UA_Server_delete(server);
+        return 7;
     }
 
     printf("EliteSCADA secure OPC UA peer ready on opc.tcp://0.0.0.0:4840\n");

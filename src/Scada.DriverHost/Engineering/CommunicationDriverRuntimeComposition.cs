89c08078aa50e9a66ba64f3ff406be032cd922b3
brunolrogerio-collab <brunolrogerio@gmail.com>
2026-08-31 00:22:35 -0300

    feat(runtime): compose host protected-material resolver

--- src/Scada.DriverHost/Engineering/CommunicationDriverRuntimeComposition.cs
+++ src/Scada.DriverHost/Engineering/CommunicationDriverRuntimeComposition.cs
@@ -1,3 +1,5 @@
+using Scada.DriverHost.Runtime;
+using Scada.Drivers.Abstractions;
 using Scada.Drivers.Mqtt;
 
 namespace Scada.DriverHost.Engineering;
@@ -10,12 +12,45 @@ namespace Scada.DriverHost.Engineering;
 public static class CommunicationDriverRuntimeComposition
 {
     public static CommunicationDriverRuntimeComponentRegistry BuildForCurrentSchema(
-        Func<IMqttClientTransport>? mqttTransportFactory = null)
+        Func<IMqttClientTransport>? mqttTransportFactory = null,
+        ICommunicationDriverProtectedMaterialResolver? hostProtectedMaterialResolver = null)
     {
+        var protectedMaterialResolver = hostProtectedMaterialResolver
+            ?? EnvironmentCommunicationDriverProtectedMaterialResolver.CreateDeterministicScopedEnvironment();
         var registry = new CommunicationDriverRuntimeComponentRegistry();
         registry.Register(new CommunicationDriverRuntimeComponentRegistration(
             new MqttCommunicationRuntimePlanner(),
-            new MqttCommunicationRuntimeFactory(mqttTransportFactory)));
+            new HostProtectedMaterialRuntimeFactory(
+                new MqttCommunicationRuntimeFactory(mqttTransportFactory),
+                protectedMaterialResolver)));
         return registry;
+    }
+
+    private sealed class HostProtectedMaterialRuntimeFactory : ICommunicationDriverRuntimeFactory
+    {
+        private readonly ICommunicationDriverRuntimeFactory _inner;
+        private readonly ICommunicationDriverProtectedMaterialResolver _resolver;
+
+        public HostProtectedMaterialRuntimeFactory(
+            ICommunicationDriverRuntimeFactory inner,
+            ICommunicationDriverProtectedMaterialResolver resolver)
+        {
+            _inner = inner ?? throw new ArgumentNullException(nameof(inner));
+            _resolver = resolver ?? throw new ArgumentNullException(nameof(resolver));
+        }
+
+        public string DriverType => _inner.DriverType;
+
+        public ICommunicationDriver Create(
+            ICommunicationDriverRuntimePlan plan,
+            CommunicationDriverRuntimeServices services)
+        {
+            ArgumentNullException.ThrowIfNull(services);
+            return _inner.Create(
+                plan,
+                services.ProtectedMaterialResolver is null
+                    ? services with { ProtectedMaterialResolver = _resolver }
+                    : services);
+        }
     }
 }

9652e34c892098ef6b8e6ec9b05ca1c69e8bde13
brunolrogerio-collab <brunolrogerio@gmail.com>
2026-08-31 01:53:59 -0300

    test(opcua): add secure open62541 peer

---
+++ interop-lab/opcua/open62541-secure/server.c
@@ -0,0 +1,86 @@
+#include "open62541.h"
+#include <signal.h>
+#include <stdio.h>
+
+#define LAB_NAMESPACE_INDEX 2
+static volatile UA_Boolean running = true;
+
+static void stopHandler(int signalNumber) { (void)signalNumber; running = false; }
+
+static UA_ByteString loadFile(const char *path) {
+    UA_ByteString data = UA_BYTESTRING_NULL;
+    FILE *file = fopen(path, "rb");
+    if(!file) return data;
+    fseek(file, 0, SEEK_END);
+    long length = ftell(file);
+    rewind(file);
+    if(length <= 0 || UA_ByteString_allocBuffer(&data, (size_t)length) != UA_STATUSCODE_GOOD) {
+        fclose(file);
+        return UA_BYTESTRING_NULL;
+    }
+    size_t read = fread(data.data, 1, data.length, file);
+    fclose(file);
+    if(read != data.length) { UA_ByteString_clear(&data); return UA_BYTESTRING_NULL; }
+    return data;
+}
+
+static UA_StatusCode addInt32Variable(UA_Server *server) {
+    UA_Int32 initialValue = 7;
+    UA_VariableAttributes attr = UA_VariableAttributes_default;
+    UA_Variant_setScalar(&attr.value, &initialValue, &UA_TYPES[UA_TYPES_INT32]);
+    attr.displayName = UA_LOCALIZEDTEXT("en-US", "Secure Counter");
+    attr.dataType = UA_TYPES[UA_TYPES_INT32].typeId;
+    attr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+    return UA_Server_addVariableNode(server,
+        UA_NODEID_STRING(LAB_NAMESPACE_INDEX, "Lab.SecureCounter"),
+        UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER),
+        UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES),
+        UA_QUALIFIEDNAME(LAB_NAMESPACE_INDEX, "Secure Counter"),
+        UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), attr, NULL, NULL);
+}
+
+int main(int argc, char **argv) {
+    if(argc != 4) {
+        fprintf(stderr, "usage: %s <server-cert.der> <server-key.der> <trusted-client-cert.der>\n", argv[0]);
+        return 2;
+    }
+    signal(SIGINT, stopHandler);
+    signal(SIGTERM, stopHandler);
+
+    UA_ByteString certificate = loadFile(argv[1]);
+    UA_ByteString privateKey = loadFile(argv[2]);
+    UA_ByteString trustedClient = loadFile(argv[3]);
+    if(!certificate.length || !privateKey.length || !trustedClient.length) {
+        fprintf(stderr, "failed to load secure peer material\n");
+        return 3;
+    }
+
+    UA_Server *server = UA_Server_new();
+    if(!server) return 4;
+    UA_ServerConfig *config = UA_Server_getConfig(server);
+    UA_StatusCode status = UA_ServerConfig_setDefaultWithSecurityPolicies(
+        config, 4840, &certificate, &privateKey, &trustedClient, 1, NULL, 0, NULL, 0);
+    UA_ByteString_clear(&certificate);
+    UA_ByteString_clear(&privateKey);
+    UA_ByteString_clear(&trustedClient);
+    if(status != UA_STATUSCODE_GOOD) {
+        fprintf(stderr, "secure server configuration failed: %s\n", UA_StatusCode_name(status));
+        UA_Server_delete(server);
+        return 5;
+    }
+
+    UA_String_clear(&config->applicationDescription.applicationUri);
+    config->applicationDescription.applicationUri = UA_STRING_ALLOC("urn:elitescada:interop:opcua:server");
+
+    UA_UInt16 namespaceIndex = UA_Server_addNamespace(server, "urn:elitescada:interop:opcua");
+    if(namespaceIndex != LAB_NAMESPACE_INDEX || addInt32Variable(server) != UA_STATUSCODE_GOOD) {
+        UA_Server_delete(server);
+        return 6;
+    }
+
+    printf("EliteSCADA secure OPC UA peer ready on opc.tcp://0.0.0.0:4840\n");
+    fflush(stdout);
+    status = UA_Server_run(server, &running);
+    UA_Server_delete(server);
+    return status == UA_STATUSCODE_GOOD ? 0 : 1;
+}

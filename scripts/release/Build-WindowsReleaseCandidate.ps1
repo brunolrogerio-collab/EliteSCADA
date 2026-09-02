b425a57a1ed126024d405dd03290a6a3a46c75f3
brunolrogerio-collab <brunolrogerio@gmail.com>
2026-09-01 22:51:22 -0300

    release: fix PowerShell candidate builder parser error

--- scripts/release/Build-WindowsReleaseCandidate.ps1
+++ scripts/release/Build-WindowsReleaseCandidate.ps1
@@ -16,7 +16,7 @@ function Invoke-Checked {
 
     & $FilePath @Arguments
     if ($LASTEXITCODE -ne 0) {
-        throw "Command failed with exit code $LASTEXITCODE: $FilePath $($Arguments -join ' ')"
+        throw "Command failed with exit code ${LASTEXITCODE}: $FilePath $($Arguments -join ' ')"
     }
 }
 

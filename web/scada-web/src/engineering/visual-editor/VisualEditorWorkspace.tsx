26c27cb8fd955294c60bb3a1d6e109ec6fe470fb
brunolrogerio-collab <brunolrogerio@gmail.com>
2026-09-03 01:47:11 -0300

    fix(c07): normalize Dynamo definitions for entire visual workspace

--- web/scada-web/src/engineering/visual-editor/VisualEditorWorkspace.tsx
+++ web/scada-web/src/engineering/visual-editor/VisualEditorWorkspace.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { EngineeringLocale } from '../i18n';
 import type { EngineeringSnapshot } from '../types';
+import { normalizeDynamoDefinitionParameterContract } from '../../runtime/visual-navigation/dynamoParameterWireContract';
 import { DynamoAuthoringCatalogProvider } from './DynamoAuthoringCatalogContext';
 import { VisualEditorWorkspace as LegacyVisualEditorWorkspace } from './VisualEditorWorkspaceLegacy';
 
@@ -13,11 +14,19 @@ export function VisualEditorWorkspace({
   locale: EngineeringLocale;
   onApplied: () => Promise<void>;
 }) {
+  const normalizedSnapshot = React.useMemo<EngineeringSnapshot>(() => ({
+    ...snapshot,
+    package: {
+      ...snapshot.package,
+      dynamos: snapshot.package.dynamos?.map(normalizeDynamoDefinitionParameterContract)
+    }
+  }), [snapshot]);
+
   return <DynamoAuthoringCatalogProvider
-    definitions={snapshot.package.dynamos ?? []}
-    tags={snapshot.package.tags ?? []}
-    visualAssets={snapshot.package.visualAssets ?? []}
+    definitions={normalizedSnapshot.package.dynamos ?? []}
+    tags={normalizedSnapshot.package.tags ?? []}
+    visualAssets={normalizedSnapshot.package.visualAssets ?? []}
   >
-    <LegacyVisualEditorWorkspace snapshot={snapshot} locale={locale} onApplied={onApplied} />
+    <LegacyVisualEditorWorkspace snapshot={normalizedSnapshot} locale={locale} onApplied={onApplied} />
   </DynamoAuthoringCatalogProvider>;
 }

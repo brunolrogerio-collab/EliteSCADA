a05d349a37a41629ee13b47b4652df74d287273d
brunolrogerio-collab <brunolrogerio@gmail.com>
2026-09-08 11:55:28 -0300

    fix(w14-c26): make Engineering field states readable

--- web/scada-web/src/main.tsx
+++ web/scada-web/src/main.tsx
@@ -16,6 +16,7 @@ import { RuntimeApplicationMount } from './runtime/application/RuntimeApplicatio
 import { HistoricalDataBrowserRuntime } from './runtime/historical-browser/HistoricalDataBrowserRuntime';
 import './styles.css';
 import './app-theme.css';
+import './engineering/engineering-control-states.css';
 import './runtime/application/runtime-operator.css';
 
 initializeAppTheme();
